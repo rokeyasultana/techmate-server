73d0865edc70fc76e5d149ae2339b4fee7a86b6d
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-25 19:44:51 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+//mongodb
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluster0.xjsrqww.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri);
 
 app.get('/', (req, res) => {
   res.send('Hello products resale website')
