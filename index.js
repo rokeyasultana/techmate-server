9c582a14bfa856010f516d265e6c28dbe624a74f
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-28 21:34:20 +0600

    seller

--- index.js
+++ index.js
@@ -87,8 +87,25 @@ app.get('/users',async(req,res)=>{
   res.send(user);
 });
 
+app.get ('/users/admin/:id',async(req,res)=>{
+  const email = req.params.email;
+  const query = {email};
+  const user = await usersCollection.findOne(query);
+  res.send({isAdmin:user?.role === 'admin'})
+
+}
+)
+
 
 app.put('/users/admin/:id',async(req,res)=>{
+
+  const decodedEmail = req.decoded.email;
+  const query = {email: decodedEmail};
+  const user = await usersCollection.findOne(query);
+
+  if(user.role !== 'admin'){
+    return res.status(403).send({message:'forbidden access'})
+  }
   const id = req.params.id;
   const filter = {_id:ObjectId(id)};
 const options = {upsert:true};
