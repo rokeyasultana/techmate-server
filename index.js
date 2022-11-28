c4f211a404fdb92e2b64f61d79945025ffc28eda
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-28 18:41:20 +0600

    verify jwt token

--- index.js
+++ index.js
@@ -18,6 +18,25 @@ const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASS}@cluste
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+function verifyJWT(req,res,next){
+  console.log('token inside verifyJWT',req.headers.authorization);
+  if(!authHeader){
+    return res.send(401).send('unauthorized access');
+  }
+  const token = authHeader.split('')[1];
+  jwt.verify( token,process.env.ACCESS_TOKEN,function(err,decoded){
+    if(err){
+      return res.status(403).send({message: 'forbidden access'})
+    };
+
+    req.decoded = decoded;
+    next();
+
+  });
+  
+}
+
+
 async function run(){
 try {
 
@@ -83,7 +102,11 @@ app.get('/users',async(req,res)=>{
 });
 
 
+
+
+
 }
+
 
 
 
